633d64fd14df9d2897e7cffe4dbe539fe284ea9c
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2023-09-11 14:28:56 +0100

    add initial tests

---
+++ routes/test.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const Product = require('../models/Product');
+const SiteContent = require('../models/SiteContent');
+const fs = require('fs');
+const path = require('path');
+const testDataString = filename => fs.readFileSync(path.join(__dirname, `../test/files/${filename}`)).toString();
+
+router.use((req, res, next) => req.session.production ? res.redirect("/") : next());
+
+router.get('/product/create', async (req, res) => {
+    try {
+        const products = JSON.parse(testDataString("products.json"));
+        const saved = await Product.create(products);
+        res.send(saved);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+});
+
+router.get('/product/delete/:id?', async (req, res) => {
+    try {
+        const result = await Product._deleteMany(req.params.id ? { _id: req.params.id } : {});
+        res.send(result);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+});
+
+router.get('/product/unit/delete/:id', async (req, res) => {
+    try {
+        await Product.deleteUnitsByIds(req.params.id);
+        res.send("Product units deleted successfully");
+    } catch(e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+});
+
+router.get('/product/list/:id?', async (req, res) => {
+    try {
+        const result = await Product.find(req.params.id ? { _id: req.params.id } : {});
+        res.send(result);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+});
+
+router.get('/site/content/update', async (req, res) => {
+    try {
+        const { home_main_image } = req.query;
+        const content = await SiteContent.findOne() || new SiteContent();
+        content.about_info = testDataString("about-info.txt");
+        content.home_main_image.url = home_main_image;
+        content.platforms = [{ name: "Twitter", url: "https://example.com" }];
+        content.platforms.push({ name: "Instagram", url: "https://example.com" });
+
+        const saved = await content.save();
+        res.send(saved);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+});
+
+router.get('/site/content/media/delete', async (req, res) => {
+    try {
+        const result = await SiteContent.deleteMedia(req.query.field);
+        res.send(result);
+    } catch(e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+});
+
+module.exports = router;
