690028999343f549511f5e51ec0e5eb0d780196b
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2023-10-04 02:55:17 +0100

    create Recipient class for MailTransporter class - to redefine data type for recipient objects

--- modules/mail-transporter.js
+++ modules/mail-transporter.js
@@ -1,16 +1,20 @@
 const path = require('path');
 const nodemailer = require('nodemailer');
 const { renderFile } = require('ejs');
-const { Document: Doc } = require('mongoose');
 const SMTPTransport = require('nodemailer/lib/smtp-transport');
 const { OAuth2 } = (require("googleapis")).google.auth;
 const { OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_REFRESH_TOKEN, NODE_ENV, DOMAIN_EMAIL } = process.env;
+
+class Recipient {
+    name = new String;
+    email = new String;
+};
 
 /** Class for processing mail transports */
 class MailTransporter {
     #recipient; #recipients; #oauth2Client;
 
-    /** @param {(Doc | Doc[])} recipient registered recipient(s) */
+    /** @param {(Recipient | Recipient[])} recipient mail recipient(s) */
     constructor(recipient) {
         this.#recipient = !Array.isArray(recipient) ? recipient : null;
         this.#recipients = Array.isArray(recipient) ? recipient : [];
@@ -50,7 +54,7 @@ class MailTransporter {
     };
 
     /**
-     * Starts SMTP process of creating and sending new emails
+     * Creates and sends new emails
      * @param {object} mail_opts contents to be applied to compose the email
      * @param {string} mail_opts.subject email subject
      * @param {string} mail_opts.message email message
@@ -64,20 +68,20 @@ class MailTransporter {
             const html = await renderFile(template, { message, recipient: this.#recipient, location_origin: MailTransporter.location_origin });
             const options = await this.#getTransportOpts();
             const from = { name: "Simbaluxe", address: DOMAIN_EMAIL };
-            const to = this.#recipient?.email || this.#recipients.map(m => m.email);
+            const to = this.#recipient?.email || this.#recipients.map(r => r.email);
             await nodemailer.createTransport(options).sendMail({ from, to, subject, html });
             cb?.();
         } catch(err) { if (!cb) throw err; cb(err) }
     };
 
-    /** @param {Doc} recipient registered recipient */
+    /** @param {Recipient} recipient mail recipient */
     setRecipient(recipient) {
         this.#recipient = recipient;
         this.#recipients = [];
         return this
     };
 
-    /** @param {Doc[]} recipients registered recipients */
+    /** @param {Recipient[]} recipients mail recipients */
     setRecipients(recipients) {
         this.#recipient = null;
         this.#recipients = recipients;
